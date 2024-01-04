f11460f5178c1ba3968eb60b846de4c6ca741e00
Moises Alves <moisesalvespc@gmail.com>
2024-01-04 16:31:29 -0300

    last commit

--- src/screens/Home/home.js
+++ src/screens/Home/home.js
@@ -10,33 +10,203 @@ export default function Home() {
   const [products, setProducts] = useState([
     {
       id: '1',
-      name: 'Coca Cola',
-      price: 19.90
+      name: 'Feijão',
+      price: 8
     },
     {
       id: '2',
-      name: 'Panetone',
-      price: 25.90
+      name: 'Oleo',
+      price: 5
     },
     {
       id: '3',
       name: 'Arroz',
-      price: 40.50
+      price: 33
     },
     {
       id: '4',
       name: 'Rufles',
-      price: 8
+      price: 12
     },
     {
       id: '5',
-      name: 'Sorvete',
-      price: 29.90
+      name: 'Sensação',
+      price: 14
     },
     {
       id: '6',
       name: 'Detergente',
       price: 2.30
+    },
+    {
+      id: '7',
+      name: 'Sal',
+      price: 2.50
+    },
+    {
+      id: '8',
+      name: 'Pipoca Micro-Ondas',
+      price: 3
+    },
+    {
+      id: '9',
+      name: 'Bombril',
+      price: 6
+    },
+    {
+      id: '10',
+      name: 'Agua Mineral',
+      price: 11
+    },
+    {
+      id: '11',
+      name: 'Suco',
+      price: 1.20
+    },
+    {
+      id: '12',
+      name: 'Açucar',
+      price: 18
+    },
+    {
+      id: '13',
+      name: 'Café',
+      price: 18
+    },
+    {
+      id: '14',
+      name: 'Bolacha Nikito',
+      price: 2.90
+    },
+    {
+      id: '15',
+      name: 'Bolacha Mabel',
+      price: 8
+    },
+    {
+      id: '16',
+      name: 'Bolacha Salpet',
+      price: 4
+    },
+    {
+      id: '17',
+      name: 'Chero do vaso',
+      price: 2
+    },
+    {
+      id: '18',
+      name: 'Amaciante Grande',
+      price: 22
+    },
+    {
+      id: '19',
+      name: 'Amaciante Pequeno',
+      price: 10.90
+    },
+    {
+      id: '20',
+      name: 'Sabão em pó',
+      price: 15
+    },
+    {
+      id: '21',
+      name: 'Margarina',
+      price: 15.50
+    },
+    {
+      id: '22',
+      name: 'Leite Condesado',
+      price: 7
+    },
+    {
+      id: '23',
+      name: 'Creme de leite',
+      price: 5.30
+    },
+    {
+      id: '24',
+      name: 'Sabonete',
+      price: 2.50
+    },
+    {
+      id: '25',
+      name: 'Creme de Cabelo',
+      price: 30
+    },
+    {
+      id: '26',
+      name: 'Shampoo',
+      price: 12.30
+    },
+    {
+      id: '27',
+      name: 'Colgate',
+      price: 4.50
+    },
+    {
+      id: '28',
+      name: 'Kotrine',
+      price: 12.30
+    },
+    {
+      id: '29',
+      name: 'Polpa',
+      price: 5.30
+    },
+    {
+      id: '30',
+      name: 'Leite',
+      price: 5.80
+    },
+    {
+      id: '31',
+      name: 'Desodorante',
+      price: 12.30
+    },
+    {
+      id: '32',
+      name: 'Papel Higienico',
+      price: 20.30
+    },
+    {
+      id: '33',
+      name: 'Caixa de Bombom',
+      price: 15
+    },
+    {
+      id: '34',
+      name: 'Caldo Kinor',
+      price: 4
+    },
+    {
+      id: '35',
+      name: 'Sazom',
+      price: 3
+    },
+    {
+      id: '36',
+      name: 'Frango Bandeja',
+      price: 22
+    },
+    {
+      id: '37',
+      name: 'Hamburguer',
+      price: 6.30
+    },
+    {
+      id: '38',
+      name: 'Pao de Forma',
+      price: 7
+    },
+    {
+      id: '39',
+      name: 'Batatinha',
+      price: 30
+    },
+    {
+      id: '40',
+      name: 'Sorvete',
+      price: 30
     },
   ]);
   const [cart, setCart] = useState([]);
--- src/screens/Home/list.js
+++ src/screens/Home/list.js
@@ -1,20 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { TouchableOpacity } from 'react-native';
-import { Container, TextProducts, ContainerProduct, Product } from './styles';
+import { ContainerProduct, TextProducts, Product } from './styles';
 import Icon from 'react-native-vector-icons/Feather';
 
-export default function List({ data, addToCart  }) {
-  const [addProduct, setAddProduct] = useState(0);
-
-  function handleAddProduct() {
-    setAddProduct(addProduct + 1);
-  }
-
+export default function List({ data, addToCart }) {
   return (
     <ContainerProduct>
       <Product>
         <TextProducts>{data.name}</TextProducts>
-        <TouchableOpacity onPress={addToCart}>
+        <TouchableOpacity onPress={() => addToCart(data)}>
           <Icon name='plus-square' size={35} color={'green'} />
         </TouchableOpacity>
       </Product>
